3a700ecea7ec4b3179d3c44d71655a87ddfcec85
Kilany <y.m.elkilany@gmail.com>
2024-03-27 00:15:43 +0200

    Add Problems: 5, 6, 8, 10

--- impl_predicates.pl
+++ impl_predicates.pl
@@ -1,3 +1,6 @@
 % do the following: :- module(impl_predicates, <egPredicate>/<numArgs>]).
 % now in your file do the following to use predicates defined here:
 % use_module(impl_predicates).
+
+
+assignList(assign, List, List).
---
+++ part2.pl
@@ -0,0 +1,87 @@
+:-consult(data).
+:-consult(impl_predicates).
+
+
+% pbl 5
+calcPriceOfOrder(CustomerName, OrderId, TotalPrice) :-
+    customer(CustomerId, CustomerName),
+    order(CustomerId, OrderId, Items),
+    calcPriceOfItems(Items, TotalPrice).
+
+calcPriceOfItems([Item|Rest], TotalPrice) :-
+    item(Item, _, Price),
+    calcPriceOfItems(Rest, SubTotal),
+    TotalPrice is Price + SubTotal.
+
+calcPriceOfItems([], 0).
+
+
+
+% pbl6
+isBoycott(Company) :-
+    boycott_company(Company, _).
+
+isBoycott(Item) :-
+    item(Item, Company, _),
+    isBoycott(Company).
+
+
+
+% pbl 8
+removeBoycottItemsFromAnOrder(CustomerName, OrderId, NewList) :-
+    customer(CustomerId, CustomerName),
+    order(CustomerId, OrderId, Items),
+    removeBoycottItemsFromList(Items, NewList, []).
+
+removeBoycottItemsFromList([Item|Rest], NewList, PlaceHolder) :-
+    isBoycott(Item),
+    removeItemFromList(Item, [Item|Rest], TempNewList1),
+    removeBoycottItemsFromList(TempNewList1, TempNewList2, PlaceHolder),
+    %NewList is TempNewList2.
+    assignList(assign, NewList, TempNewList2).
+
+removeBoycottItemsFromList([Item|Rest], NewList, PlaceHolder) :-
+    \+ isBoycott(Item),
+    removeBoycottItemsFromList(Rest, TempNewList, PlaceHolder),
+    %NewList is [Item|TempNewList].
+    assignList(assign, NewList, [Item|TempNewList]).
+
+removeBoycottItemsFromList([], PlaceHolder, PlaceHolder).
+
+removeItemFromList(Item, [Head|Rest], [Head|NewList]) :-
+    removeItemFromList(Item, Rest, NewList).
+
+removeItemFromList(Item, [Item|NewList], NewList).
+
+
+% pbl 10
+calcPriceAfterReplacingBoycottItemsFromAnOrder(CustomerName, OrderId, NewList, TotalPrice) :- 
+    customer(CustomerId, CustomerName),
+    order(CustomerId, OrderId, Items),
+    replaceBoycottItemsInList(Items, NewList),
+    calcPriceOfItems(NewList, TotalPrice).
+
+
+replaceBoycottItemsInList([Item|Rest], NewList) :- 
+    isBoycott(Item),
+    removeItemFromList(Item, [Item|Rest], ListAfterRemove),
+    alternative(Item, AlternativeItem),
+    assignList(assign, [AlternativeItem|ListAfterRemove], ListAfterReplace),
+    replaceBoycottItemsInList(ListAfterReplace, NewList).
+
+
+replaceBoycottItemsInList([Item|Rest], NewList) :-
+    \+ isBoycott(Item),
+    replaceBoycottItemsInList(Rest, TempNewList),
+    assignList(assign, NewList, [Item|TempNewList]).
+
+
+replaceBoycottItemsInList([Item|Rest], NewList) :-
+    isBoycott(Item),
+    removeItemFromList(Item, [Item|Rest], ListAfterRemove),
+    \+ alternative(Item, AlternativeItem),
+    replaceBoycottItemsInList(ListAfterRemove, NewList).
+
+
+replaceBoycottItemsInList([], NewList) :-
+    assignList(assign, NewList, []).
